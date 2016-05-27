0d2b6a88b168473e84dbd50f19ed601a02d9e37d
Amir Bawab <amirbawab@gmail.com>
2016-05-26 22:45:26 -0400

    Moved GUI integration to a new file

---
+++ easycc/src/main/java/GuiIntegration.java
@@ -0,0 +1,141 @@
+import data.GenericTable;
+import data.LexicalAnalysisRow;
+import data.structure.ConsoleData;
+import helper.LexicalHelper;
+import listener.DevGuiListener;
+import token.AbstractToken;
+import token.ErrorToken;
+import token.LexicalToken;
+import utils.StringUtilsPlus;
+
+import javax.swing.*;
+
+public class GuiIntegration implements DevGuiListener {
+
+    private LexicalAnalyzer lexicalAnalyzer;
+
+    public GuiIntegration(LexicalAnalyzer lexicalAnalyzer){
+        this.lexicalAnalyzer = lexicalAnalyzer;
+    }
+
+    @Override
+    public void lexicalAnalysis(String text) {
+        lexicalAnalyzer.analyzeText(text);
+    }
+
+    @Override
+    public ConsoleData<LexicalAnalysisRow> getLexicalAnalyzerOutput() {
+
+        ConsoleData<LexicalAnalysisRow> rows = new ConsoleData<>();
+        for(int row = 0; row < lexicalAnalyzer.getTokens().size(); row++) {
+
+            // Get current
+            AbstractToken token = lexicalAnalyzer.getTokens().get(row);
+
+            // If not error
+            if(token instanceof LexicalToken)
+                rows.add(new LexicalAnalysisRow(token.getToken(), token.getValue(), token.getRow(), token.getCol(), token.getPosition()));
+        }
+
+        return rows;
+    }
+
+    @Override
+    public ConsoleData<LexicalAnalysisRow> getLexicalAnalyzerError() {
+
+        ConsoleData<LexicalAnalysisRow> rows = new ConsoleData<>();
+        for(int row = 0; row < lexicalAnalyzer.getTokens().size(); row++) {
+
+            // Get current
+            AbstractToken token = lexicalAnalyzer.getTokens().get(row);
+
+            // If error
+            if(token instanceof ErrorToken) {
+                rows.add(new LexicalAnalysisRow(token.getToken(), token.getValue(), token.getRow(), token.getCol(), token.getPosition(), LexicalHelper.tokenMessage(token)));
+            }
+        }
+
+        return rows;
+    }
+
+    @Override
+    public void parse() {
+
+    }
+
+    @Override
+    public Object[][] getParserOutput() {
+        return new Object[0][];
+    }
+
+    @Override
+    public Object[][] getParserError() {
+        return new Object[0][];
+    }
+
+    @Override
+    public long getLexicalAnalysisTime() {
+        return lexicalAnalyzer.getProcessTime();
+    }
+
+    @Override
+    public long getParserTime() {
+        return 0;
+    }
+
+    @Override
+    public boolean doesCompile() {
+        return false;
+    }
+
+    @Override
+    public Object[][][] getSymbolTables() {
+        return new Object[0][][];
+    }
+
+    @Override
+    public String getSymbolTableName(int id) {
+        return null;
+    }
+
+    @Override
+    public Object[][] getSemanticErrors() {
+        return new Object[0][];
+    }
+
+    @Override
+    public JPanel getParserTree() {
+        return null;
+    }
+
+    @Override
+    public String getGeneratedCode() {
+        return null;
+    }
+
+    @Override
+    public void setStateTable(GenericTable genericTable) {
+        genericTable.setHeader(StringUtilsPlus.convertStringArrayToObjectArray(lexicalAnalyzer.getStateTransitionTable().prettifyStateTransitionTableHeader()));
+        genericTable.setData(StringUtilsPlus.convertStringTableToObjectTable(lexicalAnalyzer.getStateTransitionTable().prettifyStateTransitionTableData()));
+    }
+
+    @Override
+    public Object[][] getParsingTable() {
+        return new Object[0][];
+    }
+
+    @Override
+    public Object[][] getParsingTableRules() {
+        return new Object[0][];
+    }
+
+    @Override
+    public Object[][] getParsingTableErrors() {
+        return new Object[0][];
+    }
+
+    @Override
+    public Object[][] getFirstAndFollowSets() {
+        return new Object[0][];
+    }
+}
