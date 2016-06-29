744f9dc7aa7fbf316c34b89c70baf945bd7ca429
Amir Bawab <amirbawab@gmail.com>
2016-06-29 19:30:08 -0400

    Added Syntax Config UT

--- syntax-analysis/src/test/java/core/config/SyntaxConfigTest.java
+++ syntax-analysis/src/test/java/core/config/SyntaxConfigTest.java
@@ -12,42 +12,82 @@ public class SyntaxConfigTest {
     }
 
     @Test
-    public void test_getDefaultMessage_success1() {
+    public void test_getDefaultMessage_LL_success1() {
         assertEquals("Default message", SyntaxConfig.getInstance().getLLSyntaxMessageConfig().getDefaultMessage());
     }
 
     @Test
-    public void test_getDefaultMessage_success2() {
+    public void test_getDefaultMessage_LL_success2() {
         assertEquals("Default message", SyntaxConfig.getInstance().getLLMessage("random1", "random2"));
     }
 
     @Test
-    public void test_getMessage_success1() {
+    public void test_getMessage_LL_success1() {
         assertEquals("message one non-terminal", SyntaxConfig.getInstance().getLLMessage("one", null));
     }
 
     @Test
-    public void test_getMessage_success2() {
+    public void test_getMessage_LL_success2() {
         assertEquals("message one non-terminal", SyntaxConfig.getInstance().getLLMessage("one", "random"));
     }
 
     @Test
-    public void test_getMessage_success3() {
+    public void test_getMessage_LL_success3() {
         assertEquals("message two non-terminal terminal", SyntaxConfig.getInstance().getLLMessage("two", "two_prime"));
     }
 
     @Test
-    public void test_getMessage_success4() {
+    public void test_getMessage_LL_success4() {
         assertEquals("Default message", SyntaxConfig.getInstance().getLLMessage("two", "random"));
     }
 
     @Test
-    public void test_getMessage_success5() {
+    public void test_getMessage_LL_success5() {
         assertEquals("message three non-terminal", SyntaxConfig.getInstance().getLLMessage("three", "random"));
     }
 
     @Test
-    public void test_getMessage_success6() {
+    public void test_getMessage_LL_success6() {
         assertEquals("message three non-terminal terminal", SyntaxConfig.getInstance().getLLMessage("three", "three_prime"));
+    }
+
+    @Test
+    public void test_getDefaultMessage_LR_success1() {
+        assertEquals("Default message", SyntaxConfig.getInstance().getLRSyntaxMessageConfig().getDefaultMessage());
+    }
+
+    @Test
+    public void test_getDefaultMessage_LR_success2() {
+        assertEquals("Default message", SyntaxConfig.getInstance().getLRMessage("random1", "random2"));
+    }
+
+    @Test
+    public void test_getMessage_LR_success1() {
+        assertEquals("message one error-key", SyntaxConfig.getInstance().getLRMessage("one", null));
+    }
+
+    @Test
+    public void test_getMessage_LR_success2() {
+        assertEquals("message one error-key", SyntaxConfig.getInstance().getLRMessage("one", "random"));
+    }
+
+    @Test
+    public void test_getMessage_LR_success3() {
+        assertEquals("message two error-key terminal", SyntaxConfig.getInstance().getLRMessage("two", "two_prime"));
+    }
+
+    @Test
+    public void test_getMessage_LR_success4() {
+        assertEquals("Default message", SyntaxConfig.getInstance().getLRMessage("two", "random"));
+    }
+
+    @Test
+    public void test_getMessage_LR_success5() {
+        assertEquals("message three error-key", SyntaxConfig.getInstance().getLRMessage("three", "random"));
+    }
+
+    @Test
+    public void test_getMessage_LR_success6() {
+        assertEquals("message three error-key terminal", SyntaxConfig.getInstance().getLRMessage("three", "three_prime"));
     }
 }
