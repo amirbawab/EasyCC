7b71ab9f5b4eb32a8fc87b56ab8dc37daf03e189
Amir Bawab <amirbawab@gmail.com>
2016-07-01 16:13:19 -0400

    Added fields to LRActionToken

--- syntax-analysis/src/main/java/token/LRActionToken.java
+++ syntax-analysis/src/main/java/token/LRActionToken.java
@@ -1,14 +1,41 @@
 package token;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Action token for LR parser
  */
 
 public class LRActionToken extends ActionToken {
     private AbstractSyntaxToken abstractSyntaxToken;
+    private String name;
+    private int root;
+    private List<Integer> children;
 
     public LRActionToken(String value) {
         super(value);
+
+        // Init components
+        children = new ArrayList<>();
+
+        // Match the pattern name/root/child1,child2,...
+        String[] parts = value.split("/");
+        if(parts.length != 3) {
+            throw new RuntimeException("Action token: " + value + " should be of the form %name/root/child1,child2,...%");
+        }
+
+        // Store the different parts
+        name = parts[0];
+
+        try {
+            root = Integer.parseInt(parts[1]);
+            for (String child : parts[2].split(",")) {
+                children.add(Integer.parseInt(child));
+            }
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Action token root and children should be of type integer");
+        }
     }
 
     public LRActionToken(LRActionToken lrActionToken) {
@@ -22,6 +49,18 @@ public class LRActionToken extends ActionToken {
 
     public void setAbstractSyntaxToken(AbstractSyntaxToken abstractSyntaxToken) {
         this.abstractSyntaxToken = abstractSyntaxToken;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRoot() {
+        return root;
+    }
+
+    public List<Integer> getChildren() {
+        return children;
     }
 
     @Override
